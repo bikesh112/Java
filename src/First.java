475392c8ccd909cb32e7f2191c9764ad9aaa1493
bikesh <getbikeshchy@gmail.com>
2022-04-19 20:20:00 +0545

    add

--- src/First.java
+++ src/First.java
@@ -1,15 +1,37 @@
 public class First {
     public static void main(String[] args) {
-
+        int maths=9;
+        int English=9;
+        int Science=7;
+        int Nepali=81;
+        int Sum=maths+ English+Science+Nepali;
+        int percent= Sum/4;
+        System.out.println(percent + "%");
+        if (percent>=70) {
+            System.out.println("first Class");
+        }else if(percent>59){
+            System.out.println("Upper Second Class");
+        }
+        else if (percent>49){
+            System.out.println("Second Class");
+        }
+        else if (percent>39){
+            System.out.println("Third Class");
+        }
+        else{
+            System.out.println("Fail");
+        }
     }
 }
-class q1{
-    public static void main(String[] args) {
 
-    }
-}
-class q2{
-    public static void main(String[] args) {
 
+// area of trianlge
+
+class Area{
+    public static void main(String[] args) {
+        double length=20;
+        double breadth=10;
+        double Area=(0.5)*(length*breadth);
+        System.out.println(Area+" square cm");
     }
 }
